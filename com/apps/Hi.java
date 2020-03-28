07ffe8daebde8183c575ca6360a725b0dfd4f4f3
pushkarsa <pushkar04cse@gmail.com>
2020-03-29 00:56:29 +0530

    'second'

--- com/apps/Hi.java
+++
@@ -1,5 +0,0 @@
-package com.apps;
-
-public class Hi {
-
-}
---
+++ src/com/apps/Hi.java
@@ -0,0 +1,8 @@
+package com.apps;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		System.out.println("hi in the main");
+	}
+}
