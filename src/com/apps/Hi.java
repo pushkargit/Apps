3a5f7179f00da7cd2d6bd6ec578ea05cb73b08b2
pushkarsa <pushkar04cse@gmail.com>
2020-04-17 03:10:34 +0530

    adding class qa to  qa

--- src/com/apps/Hi.java
+++ src/com/apps/Hi.java
@@ -5,5 +5,6 @@ public class Hi {
 	public static void main(String[] args) {
 		System.out.println("hi in the main");
 		System.out.println("i am in the qa");
+		System.out.println("adding hi in QA");
 	}
 }
--- src/com/apps/QA.java
+++ src/com/apps/QA.java
@@ -1,4 +1,7 @@
 package com.apps;
 
 public class QA {
+    public static void main(String[] args) {
+        System.out.println("adding main method");
+    }
 }
